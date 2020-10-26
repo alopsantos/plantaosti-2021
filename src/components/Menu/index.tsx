57a85c356434cc94392dcd878c313d2591d678fe
Anderson Lopes dos Santos <lopscorp@iMac-de-Anderson.local>
2020-10-26 00:46:13 -0300

    BugFix :bug: Corrigido problemas com os svg

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { Container } from '../../styles/pages/Menu'
 
-import Farmacia from '../../assets/images/farmacias.svg'
+import Farmacia from '../../assets/images/farmacias.svg';
 import Plantao from '../../assets/images/plantao.svg'
 import Plantoes from '../../assets/images/plantoes.svg'
 
@@ -10,17 +10,17 @@ export default function Menu() {
       <ul>
         <li>
           <a href="#">
-            <Plantao width={46} height={46} />
+            <Plantao />
           </a>
         </li>
         <li>
           <a href="#">
-            <Plantoes width={46} height={46} />
+            <Plantoes />
           </a>
         </li>
         <li>
           <a href="#">
-            <Farmacia width={46} height={46} />
+            <Farmacia />
           </a>
         </li>
       </ul>
